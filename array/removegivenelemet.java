79685e8ed7147670eb4f2e162aacc958d42f7710
bhavadeep reddy <bhavadeep2003@gmail.com>
2023-06-15 21:41:01 -0700

    commit

--- array/removegivenelemet.java
+++ array/removegivenelemet.java
@@ -1,7 +1,13 @@
- int j =0;
+ class Solution {
+    public int removeElement(int[] nums, int val) {
+    int j =0;
         for(int i=0; i<nums.length; i++){
             if(nums[i] != val){
-                nums[j++] = nums[i];
+                nums[j] = nums[i];
+                j++;
             }
         }
         return j;
+   
+    }
+}
